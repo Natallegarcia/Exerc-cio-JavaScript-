150b38e8a4b018155e87e4475c099142c79d66e0
Nátalle Garcia <natallegarcia2015@gmail.com>
2022-07-20 16:55:54 -0300

    primeiro commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Exercício desenvolvido em JavaScript proposto pelo curso de Introdução ao JavaScript e utilização de Operadores Lógicos. 
+Nesse exeercício utilizai dados ficticios. 
---
+++ credenciaisValidacao.js
@@ -0,0 +1,6 @@
+let login = 'natallegarcia';
+let senha = 'natalle03';
+let verificaCredenciais; 
+
+verificaCredenciais = login === 'natallegarcia' && senha === 'natalle03'
+console.log (verificaCredenciais); 
